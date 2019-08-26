763a081423f271640d076da9501e168d0477e8fd
jDanielDetes <jdanieldetes@gmail.com>
2019-08-26 18:42:51 -0400

    sign up page 70% complete

---
+++ app/src/main/java/com/example/haircare/Signup.kt
@@ -0,0 +1,64 @@
+package com.example.haircare
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Signup : Fragment() {
+
+    override fun onAttach(context: Context?) {
+
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater!!.inflate(R.layout.sign_up_window,container,false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+
+        super.onStart()
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+    }
+
+    override fun onPause() {
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+
+        super.onDetach()
+    }
+
+}

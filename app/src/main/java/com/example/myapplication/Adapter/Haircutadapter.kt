5663fc19e7703e55a1e5ddaadccf5d1228bd2721
TylerChristian711 <tylerc71197@gmail.com>
2019-08-26 21:36:25 -0400

    recyleview just need data now

--- app/src/main/java/com/example/myapplication/Adapter/Haircutadapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter/Haircutadapter.kt
@@ -1,17 +1,20 @@
 package com.example.myapplication.Adapter
 
 import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
 import com.example.myapplication.model.haircuts
 import kotlinx.android.synthetic.main.buildadapter.view.*
 
 class Haircutadapter(val data: MutableList<haircuts>): RecyclerView.Adapter<Haircutadapter.ViewHolder>() {
 
 
-    class ViewHolder(view:ViewGroup): RecyclerView.ViewHolder(view){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val profile: ImageView = view.profile_image
         val  hair1 : ImageView = view.cut_1
         val hair2 : ImageView = view.cut_2
@@ -21,17 +24,20 @@ class Haircutadapter(val data: MutableList<haircuts>): RecyclerView.Adapter<Hair
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+      val viewGroup =  LayoutInflater.from(parent.context).inflate(R.layout.buildadapter,parent, false)
+       return ViewHolder(viewGroup)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return data.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       holder.profile.setImageDrawable(data[position].profilepic)
+       holder.hair1.setImageDrawable(data[position].cut1)
+       holder.hair2.setImageDrawable(data[position].cut2)
+       holder.hair3.setImageDrawable(data[position].cut3)
+       holder.text.text = data[position].bio
     }
 
 }

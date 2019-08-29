c1f5f45df26c0603252afaf441b1784358a88284
jDanielDetes <jdanieldetes@gmail.com>
2019-08-29 15:34:26 -0400

    upvote/downvote 30% done

--- app/src/main/java/com/example/myapplication/UI/recyclertest.kt
+++ app/src/main/java/com/example/myapplication/UI/recyclertest.kt
@@ -16,7 +16,7 @@ class recyclertest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recycler)
-           
+
 
 
         HairRepo.createhairlist()
--- app/src/main/java/com/example/myapplication/model/haircuts.kt
+++ app/src/main/java/com/example/myapplication/model/haircuts.kt
@@ -3,5 +3,6 @@ package com.example.myapplication.model
 import android.graphics.drawable.Drawable
 
 
-data class haircuts(val profilepic:Int, val cut1:Int, val cut2:Int,val cut3: Int, val bio:String)
+data class haircuts(val profilepic:Int, val cut1:Int, val cut2:Int,val cut3: Int, val bio:String,
+                    val upvote: Int = 0, val downvote: Int = 0)
 // what our recyclerview holds
